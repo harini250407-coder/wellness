8e3b42505a9f0550deb26e5f64084a00a7195bf2
Harini S <harini250407@gmail.com>
2025-09-09 16:54:40 +0530

    Version1

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#typed', {
+    strings: ["AI Career Advisor", "Discover Your Best Career", "Shape Your Future with AI"],
+    typeSpeed: 50,
+    backSpeed: 30,
+    backDelay: 1500,
+    loop: true
+  });
